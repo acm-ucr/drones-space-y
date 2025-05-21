120d757c79171c2c781d0c3e9001df40ff8f7901
Pratheek Sunilkumar <66023535+pratheek28@users.noreply.github.com>
2025-05-21 14:49:36 -0700

    Update imuTest.py
    changed imuTest.py to detect when drone is leaning forward

--- imu/imuTest.py
+++ imu/imuTest.py
@@ -3,12 +3,25 @@ from smbus import SMBus
 import time
 
 sensor = mpu6050(0x68)
+print("Sensor found!")
+
+normal = []
 
 while(True):
-    time.sleep(0.5)
+    time.sleep(1)
     
     gyro = sensor.get_gyro_data()
+    if not normal:
+        normal.append(gyro['y'])
     accel = sensor.get_accel_data()
 
-    print("Accelrometer: ", accel)
-    print("Gyroscope: ", gyro)
+    if (abs(gyro['y'] - normal[0]) > 10):
+        normal[0] = gyro['y']
+        print("Leaning Forward...")
+    else:
+        print("Normal/leaning backwards")
+
+    # print("Accelrometer: ", accel)
+    # print("Gyroscope: ", gyro)
+    # print()
+    # print()
